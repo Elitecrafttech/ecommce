59ef3ce3915ceacf9467a4c7132a129505046559
Elitecrafttech <Elitecrafttech@gmail.com>
2025-03-04 13:20:52 +0100

    updt

--- src/component/Testimonials/Testimonials.jsx
+++ src/component/Testimonials/Testimonials.jsx
@@ -76,7 +76,7 @@ const Testimonials = () => {
     <div className='py-10 mb-10'>
         <div className='container'>
             <div className='text-center mb-10 max-w-[600px] mx-auto'>
-                <p data-aos="fade-up" className='text-sm text-primary'>What our customers are saying</p>
+                <p data-aos="fade-up" className='text-sm text-primary'>- What our customers are saying -</p>
                 <h1 data-aos="fade-up" className='text-3xl font-bold'>Testimonial</h1>
                 <p data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero voluptate consequuntur ad?</p>
             </div>
